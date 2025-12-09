9558cb0439010c59139a46bb11cf9f62840c6a16
Boris Brezillon <boris.brezillon@collabora.com>
2025-12-09 20:30:05 +0100

    avcodec: Add V4L2 Request API vp9 hwaccel
    Add a V4L2 Request API hwaccel for VP9.
    
    Support for VP9 is enabled when Linux kernel headers declare the
    control id V4L2_CID_STATELESS_VP9_FRAME, added in v5.17.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Co-developed-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Co-developed-by: Jonas Karlman <jonas@kwiboo.se>
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -168,6 +168,7 @@ static int update_size(AVCodecContext *avctx, int w, int h)
                      CONFIG_VP9_D3D11VA_HWACCEL * 2 + \
                      CONFIG_VP9_D3D12VA_HWACCEL + \
                      CONFIG_VP9_NVDEC_HWACCEL + \
+                     CONFIG_VP9_V4L2REQUEST_HWACCEL + \
                      CONFIG_VP9_VAAPI_HWACCEL + \
                      CONFIG_VP9_VDPAU_HWACCEL + \
                      CONFIG_VP9_VIDEOTOOLBOX_HWACCEL + \
@@ -211,6 +212,9 @@ static int update_size(AVCodecContext *avctx, int w, int h)
 #endif
 #if CONFIG_VP9_VULKAN_HWACCEL
             *fmtp++ = AV_PIX_FMT_VULKAN;
+#endif
+#if CONFIG_VP9_V4L2REQUEST_HWACCEL
+            *fmtp++ = AV_PIX_FMT_DRM_PRIME;
 #endif
             break;
         case AV_PIX_FMT_YUV420P12:
@@ -1953,6 +1957,9 @@ const FFCodec ff_vp9_decoder = {
 #endif
 #if CONFIG_VP9_VULKAN_HWACCEL
                                HWACCEL_VULKAN(vp9),
+#endif
+#if CONFIG_VP9_V4L2REQUEST_HWACCEL
+                               HWACCEL_V4L2REQUEST(vp9),
 #endif
                                NULL
                            },
